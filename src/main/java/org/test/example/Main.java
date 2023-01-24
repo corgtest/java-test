c602a0249ba65992dc69041249f1e33895053d44
Bogdana <bogdana.kushnir@sonarsource.com>
2023-01-24 10:01:43 +0100

    testing

--- src/main/java/org/test/example/Main.java
+++ src/main/java/org/test/example/Main.java
@@ -5,5 +5,19 @@ public class Main {
   public static void main(String[] args) throws Exception{
     String s;
     System.out.println("Hello world!");
+    int i = 0;
+    boolean f;
+
+    if (i==0){
+      f = true;
+    }
+
+    // sfsfsdf
+    ///sdfsf
+    ///sdfsdf
+
+    String p;
+    String p1;
+
   }
 }
